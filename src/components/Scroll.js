62cdaeeec7dfeeefbb816c518d93fbd26608e94f
AnnaZaychikova <annzaychikova@gmail.com>
2021-02-07 12:25:33 +0700

    Added hooks

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Scroll = (props) => {
-    return (
+const Scroll = (props) => (
         <div style={{ overflowY: 'scroll', border: '5px solid black', height: '800px' }}>
             {props.children}
         </div>
     );
-}
 
 export default Scroll;
